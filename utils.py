9370a87c88bf41b309da694833c81845cc960d50
apchenstu <apchen.stu@gmail.com>
2023-09-27 08:08:14 +0200

    fix nan in tv

--- utils.py
+++ utils.py
@@ -147,6 +147,7 @@ class TVLoss(nn.Module):
         w_x = x.size()[3]
         count_h = self._tensor_size(x[:,:,1:,:])
         count_w = self._tensor_size(x[:,:,:,1:])
+        count_w = max(count_w, 1)
         h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
         w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
         return self.TVLoss_weight*2*(h_tv/count_h+w_tv/count_w)/batch_size
